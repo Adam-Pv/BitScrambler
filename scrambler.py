5ff43aa0c35bd2bf6ce5d7d1617889bc24b245a1
Adam-Pv <adamjpeavey@gmail.com>
2018-01-23 19:03:06 -0800

    added cytpro amount

--- scrambler.py
+++ scrambler.py
@@ -4,13 +4,31 @@ import time
 from datetime import datetime
 from progress.bar import Bar, IncrementalBar
 
-timeframe = raw_input("how many minutes would you like to schedule this for? : ")
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+#
+# Sets up the timeframe, amount of addresses, and addresses.
+#
+# 
+sendAmount = raw_input("How much crypto are you sending? ")
+#while sendAmount.isdigit() == False or isFloat(sendAmount) == False:
+while isFloat(sendAmount) == False:
+	print("Invalid entry!")
+	sendAmount = raw_input("How much crypto are you sending? ")
+
+timeframe = raw_input("How many minutes would you like to schedule this for? : ")
 	
 while timeframe.isdigit() == False or int(timeframe) == 0:
 	print ("Invalid entry!")
 	timeframe = raw_input("How many minutes would you like to schedule this for? : ")
 
-addresses = raw_input("how many addesses would you like to send payment too? : ")
+addresses = raw_input("How many addesses would you like to send payment too? : ")
 while int(addresses) > 100 or int(addresses) == 0:
 	print ("Addresses limited to 30!")
 	addresses = raw_input("how many addesses would you like to send payment too? : ")
@@ -27,9 +45,15 @@ for x in range (0, int(addresses)):
 
 	sendTime = random.randint(1 ,timeframe-1)
 	publicKeys[sendTime] = newKey
+
 items = publicKeys.items()
 items.sort(reverse=True)
 
+
+#
+# Sends the actual transactions
+#
+#
 helper = 0
 timeLapsed = 0
 
@@ -63,12 +87,16 @@ for i in range(0, items[0][0]+1):
 		sentTime.append(currentTime)
 
 		helper = helper + 1
+		print ("TRANSACTION SENT")
 
 		if int(helper) == int(addresses):
 			bar.finish()
 			break
 
-
+#
+# Transactional report
+#
+#
 print ""
 print "FINISHED"
 now = datetime.now()
